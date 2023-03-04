f49ee5ca1a0832de9a474502aad3560a714bf2ce
weidelix <dev.mesina@gmail.com>
2023-03-04 10:33:15 +0800

    Added "no manga to update" message

--- lib/sources/manga_updater.dart
+++ lib/sources/manga_updater.dart
@@ -33,40 +33,59 @@ class _MangaUpdaterState extends State<MangaUpdater> {
   Widget build(BuildContext context) {
     final appTheme = context.read<AppTheme>();
 
-    return Builder(
-      builder: (context) {
-        WidgetsBinding.instance.addPostFrameCallback(_refreshMangas);
-        double mangaCount = MangaManager().mangas.length.toDouble();
+    double mangaCount = MangaManager().mangas.length.toDouble();
 
-        return InfoBar(
-            style: InfoBarThemeData(
-                decoration: (severity) =>
-                    BoxDecoration(color: FluentTheme.of(context).cardColor),
-                icon: (severity) => fui.FluentIcons.arrow_clockwise_24_regular),
-            title: const Text(''),
-            content: Container(
-              height: 35,
-              constraints: const BoxConstraints(minWidth: 300, maxWidth: 300),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  ValueListenableBuilder(
-                      valueListenable: currentManga,
-                      builder: (context, value, child) => Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              Text(
-                                "Updating library...",
-                                maxLines: 1,
-                                overflow: TextOverflow.ellipsis,
-                                style: appTheme.bodyStrong,
-                              ),
-                              Text(
-                                  ' ${mangaIndex.value.toInt() + 1}/${mangaCount.toInt()}')
-                            ],
-                          )),
-                  ValueListenableBuilder(
+    if (mangaCount > 0) {
+      WidgetsBinding.instance.addPostFrameCallback(_refreshMangas);
+    } else {
+      WidgetsBinding.instance.addPostFrameCallback((duration) async {
+        await Future.delayed(const Duration(seconds: 5));
+        widget.onFinish();
+      });
+    }
+
+    return InfoBar(
+        severity: mangaCount > 0 ? InfoBarSeverity.info : InfoBarSeverity.error,
+        style: InfoBarThemeData(
+            decoration: (severity) =>
+                BoxDecoration(color: FluentTheme.of(context).cardColor),
+            icon: (severity) {
+              if (severity == InfoBarSeverity.error) {
+                return fui.FluentIcons.error_circle_24_regular;
+              }
+
+              return fui.FluentIcons.arrow_clockwise_24_regular;
+            }),
+        title: const Text(''),
+        content: Container(
+          height: 40,
+          constraints: const BoxConstraints(minWidth: 300, maxWidth: 300),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              ValueListenableBuilder(
+                  valueListenable: currentManga,
+                  builder: (context, value, child) => Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            mangaCount > 0
+                                ? "Checking for new chapters..."
+                                : "No mangas to update",
+                            maxLines: 1,
+                            overflow: TextOverflow.ellipsis,
+                            style: appTheme.bodyStrong,
+                          ),
+                          mangaCount > 0
+                              ? Text(
+                                  ' ${mangaIndex.value.toInt() + 1}/${mangaCount.toInt()}',
+                                  style: appTheme.caption)
+                              : const SizedBox.shrink()
+                        ],
+                      )),
+              mangaCount > 0
+                  ? ValueListenableBuilder(
                       valueListenable: mangaIndex,
                       builder: (context, value, child) => SizedBox(
                             width: double.infinity,
@@ -74,11 +93,11 @@ class _MangaUpdaterState extends State<MangaUpdater> {
                                 value: (100.0 / mangaCount) *
                                     ((value as double) + 1)),
                           ))
-                ],
-              ),
-            ));
-      },
-    );
+                  : Text('Add mangas to your library to update them',
+                      style: appTheme.caption)
+            ],
+          ),
+        ));
   }
 
   void _refreshMangas(Duration timestamp) async {
@@ -91,7 +110,7 @@ class _MangaUpdaterState extends State<MangaUpdater> {
       final chapters = manga.chapters;
       try {
         List<Chapter> newChapters =
-            await sources[manga.source]!.fetchChapters(manga.url);
+            await sources[manga.source]!.getChapters(manga.url);
         for (var chapter in newChapters) {
           if (chapters.singleWhereOrNull((c) => c.url == chapter.url) == null) {
             chapters.add(chapter);
