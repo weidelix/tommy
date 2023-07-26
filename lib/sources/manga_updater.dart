6dd02d09be70190485eae5f6da4bfa404b96c513
weidelix <dev.mesina@gmail.com>
2023-07-26 20:04:23 +0800

    Fixed padding

--- lib/sources/manga_updater.dart
+++ lib/sources/manga_updater.dart
@@ -64,42 +64,45 @@ class _MangaUpdaterState extends State<MangaUpdater> {
           content: Container(
             height: 40,
             constraints: const BoxConstraints(minWidth: 300, maxWidth: 300),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                ValueListenableBuilder(
-                    valueListenable: currentManga,
-                    builder: (context, value, child) => Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Text(
+            child: Padding(
+              padding: const EdgeInsets.only(right: 8.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  ValueListenableBuilder(
+                      valueListenable: currentManga,
+                      builder: (context, value, child) => Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Text(
+                                mangaCount > 0
+                                    ? "Checking for new chapters..."
+                                    : "No mangas to update",
+                                maxLines: 1,
+                                overflow: TextOverflow.ellipsis,
+                                style: appTheme.bodyStrong,
+                              ),
                               mangaCount > 0
-                                  ? "Checking for new chapters..."
-                                  : "No mangas to update",
-                              maxLines: 1,
-                              overflow: TextOverflow.ellipsis,
-                              style: appTheme.bodyStrong,
-                            ),
-                            mangaCount > 0
-                                ? Text(
-                                    ' ${mangaIndex.value.toInt() + 1}/${mangaCount.toInt()}',
-                                    style: appTheme.caption)
-                                : const SizedBox.shrink()
-                          ],
-                        )),
-                mangaCount > 0
-                    ? ValueListenableBuilder(
-                        valueListenable: mangaIndex,
-                        builder: (context, value, child) => SizedBox(
-                              width: double.infinity,
-                              child: ProgressBar(
-                                  value: (100.0 / mangaCount) *
-                                      ((value as double) + 1)),
-                            ))
-                    : Text('Add mangas to your library to update them',
-                        style: appTheme.caption)
-              ],
+                                  ? Text(
+                                      ' ${mangaIndex.value.toInt() + 1}/${mangaCount.toInt()}',
+                                      style: appTheme.caption)
+                                  : const SizedBox.shrink()
+                            ],
+                          )),
+                  mangaCount > 0
+                      ? ValueListenableBuilder(
+                          valueListenable: mangaIndex,
+                          builder: (context, value, child) => SizedBox(
+                                width: double.infinity,
+                                child: ProgressBar(
+                                    value: (100.0 / mangaCount) *
+                                        ((value as double) + 1)),
+                              ))
+                      : Text('Add mangas to your library to update them',
+                          style: appTheme.caption)
+                ],
+              ),
             ),
           )),
     );
